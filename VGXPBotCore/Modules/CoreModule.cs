5f127d882db1472df1a803b12c4f4a3ec47999ca
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2019-03-28 15:35:24 -0600

    Add NuGet Packages and add core functions for bot connection
    Added all the necessary NuGet Packages to the project.
    Added the core connection functionality to the bot
    (included the ability to create and delete DB servers
    on server join and server leave respectively)

---
+++ VGXPBotCore/Modules/CoreModule.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Addons.Interactive;
+
+using Newtonsoft.Json;
+
+using System.Data.SQLite;
+
+namespace VGXPBotCore.Modules
+{
+  public static class CoreModule
+  {
+
+    //Create a simple embed object
+    public static EmbedBuilder SimpleEmbed(
+      Color _color,
+      string _author,
+      string _description,
+      string _iconUrl)
+    {
+
+      //Create & set embed object
+      var embed = new EmbedBuilder();
+
+      //Set stripe color
+      embed.Color = _color;
+
+      //Set embed content
+      embed
+        .WithAuthor(_author, _iconUrl)
+        .WithDescription(_description);
+
+      return embed;
+    }
+
+    //Create Server Database
+    public static void CreateDB(
+      string _serverId)
+    {
+
+      //In case that the database file don't exist
+      if (!File.Exists("Databases/"+_serverId))
+      {
+
+        //In case that the database folder don't exist
+        if(!Directory.Exists("Databases"))
+        {
+
+          //Create directory
+          Directory.CreateDirectory("Databases");
+        }
+
+        //Create Database file
+        using (var stream = File.Create("Databases/" + _serverId)) { }
+
+        //Create and set the database connection
+        SQLiteConnection sQLiteConnection =
+          new SQLiteConnection($"Data Source = Databases/{_serverId}; Version = 3;");
+
+        //Open the connection
+        sQLiteConnection.Open();
+
+        //Create all the tables needed for the database
+        SQLiteCommand sQLiteCommand = new SQLiteCommand(
+          "CREATE TABLE settings (" +
+          "role text NOT NULL, " +
+          "notifications integer NOT NULL, " +
+          "notificationChannelId integer NOT NULL" +
+          ");" +
+          "CREATE TABLE users (" +
+          "id integer NOT NULL, " +
+          "name text NOT NULL, " +
+          "region text NOT NULL, " +
+          "actualXP integer NOT NULL, " +
+          "lastXP integer NOT NULL" +
+          ");", sQLiteConnection);
+
+        //Execute the query
+        sQLiteCommand.ExecuteNonQuery();
+
+        //Close the connection
+        sQLiteConnection.Close();
+      }
+    }
+
+    public static void DeleteDB(string _serverId)
+    {
+
+      //Delete the specified file
+      File.Delete("Databases/" + _serverId);
+    }
+  }
+}
--- VGXPBotCore/Program.cs
+++ VGXPBotCore/Program.cs
@@ -1,12 +1,173 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Addons.Interactive;
 
 namespace VGXPBotCore
 {
   class Program
   {
-    static void Main(string[] args)
+    //Define discord socket client
+    private DiscordSocketClient _client;
+
+    //Define command service
+    private CommandService _commands;
+
+    //Define service provider
+    private IServiceProvider _services;
+
+    //Bot token
+    string token = GetToken("token.txt");
+
+    public static void Main(string[] argss)
+      => new Program().MainAsync().GetAwaiter().GetResult();
+
+    public async Task MainAsync()
     {
-      Console.WriteLine("Hello World!");
+
+      //Set socket client
+      _client = new DiscordSocketClient();
+
+      //Set command service
+      _commands = new CommandService();
+
+      //configure services
+      _services = ConfigureServices();
+
+      //Set client log
+      _client.Log += Log;
+
+      //Set Client Joined Guild
+      _client.JoinedGuild += JoinedGuild;
+
+      _client.LeftGuild += LeftGuild;
+
+      //Set game
+      await _client.SetGameAsync("~help for commands");
+      //await _client.SetGameAsync("On development");
+
+      //Install the commands
+      await InstallCommandsAsync();
+
+      //Set login with bot token
+      await _client.LoginAsync(TokenType.Bot, token);
+
+      //Start connection
+      await _client.StartAsync();
+
+      //Block this task until program is closed
+      await Task.Delay(-1);
+    }
+
+    public static string GetToken(string _file)
+    {
+
+      //Create & set the token bot file
+      StreamReader readFile = new StreamReader(_file);
+
+      //Create string reader
+      string line;
+
+      //Read token
+      line = readFile.ReadLine();
+      //Console.WriteLine("token: " + line);
+
+      //Close connection
+      readFile.Close();
+
+      return line;
+    }
+
+    public async Task InstallCommandsAsync()
+    {
+
+      //Hook the MessageReceived Event into our Command Handler
+      _client.MessageReceived += HandleCommandAsync;
+
+      //Discover all of the commands in this assembly and load them.
+      await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    private async Task HandleCommandAsync(SocketMessage messageParam)
+    {
+
+      // Don't process the command if it was a System Message
+      var message = messageParam as SocketUserMessage;
+      if (message == null) return;
+
+      //Create a number to track where the prefix ends and the command begins
+      int argPos = 0;
+
+      //Determine if the message is a command with prefix
+      if (!(message.HasCharPrefix('~', ref argPos) ||
+        message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+
+      //Create a Command Context
+      var context = new SocketCommandContext(_client, message);
+
+      //Execute the command. (result does not indicate a return value, 
+      //rather an object stating if the command executed successfully)
+      var result = await _commands.ExecuteAsync(context, argPos, _services);
+      if (!result.IsSuccess)
+      {
+
+        var embed = Modules.CoreModule.SimpleEmbed(
+          Color.Red,
+          "Error",
+          result.ErrorReason,
+          context.Client.CurrentUser.GetAvatarUrl());
+
+        await context.Channel.SendMessageAsync("", false, embed.Build());
+      }
+    }
+    private IServiceProvider ConfigureServices()
+    {
+      return new ServiceCollection()
+        .AddSingleton(_client)
+        .AddSingleton(_commands)
+        .AddSingleton<InteractiveService>()
+        .BuildServiceProvider();
+      ;
+    }
+
+    //Logging method
+    private Task Log(LogMessage msg)
+    {
+
+      //Write on console
+      Console.WriteLine(msg.ToString());
+
+      //Return task as completed
+      return Task.CompletedTask;
+    }
+
+    //Joined Guild method
+    private Task JoinedGuild(SocketGuild server)
+    {
+
+      //Create the server database
+      Modules.CoreModule.CreateDB($"{server.Id}.db");
+
+      //Return task as completed
+      return Task.CompletedTask;
+    }
+
+    //Left Guild method
+    private Task LeftGuild(SocketGuild server)
+    {
+
+      //Delete the server database
+      Modules.CoreModule.DeleteDB($"{server.Id}.db");
+      
+      //Return task as completed
+      return Task.CompletedTask;
     }
   }
 }
