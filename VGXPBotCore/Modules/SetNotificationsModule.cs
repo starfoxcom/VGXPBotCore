f89c748a03b329267145d99c7f8bcad73ea26508
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2020-02-18 21:59:21 -0600

    Implement style guide to SetNotificationsModule.cs

--- VGXPBotCore/Modules/SetNotificationsModule.cs
+++ VGXPBotCore/Modules/SetNotificationsModule.cs
@@ -12,64 +12,65 @@ using Discord.Addons.Interactive;
 
 using System.Data.SQLite;
 
-namespace VGXPBotCore.Modules
-{
+namespace VGXPBotCore.Modules {
+  /// <summary>
+  /// Class which contains commands to set the bot notifications.
+  /// </summary>
   public class SetNotificationsModule : InteractiveBase
   {
-
+    /// <summary>
+    /// Command task which changes the notification settings on or off.
+    /// </summary>
+    /// <param name="_boolean">The bool to change the notifications.</param>
+    /// <remarks>
+    /// The user requires either be able to kick members on the server or be the bot owner.
+    /// </remarks>
     [RequireUserPermission(GuildPermission.KickMembers, Group = "Permission")]
     [RequireOwner(Group = "Permission")]
     [Command("setnotifications")]
     [Summary("`Sets` the notifications of the bot.")]
     [Alias("sn")]
-    public async Task SetNotificationsAsync(bool boolean)
+    public async Task
+    SetNotificationsAsync(bool _boolean)
     {
-
-      //On true
-      if (boolean)
-      {
-
-        //Execute query
+      //On true.
+      if (_boolean) {
+        //Execute query.
         CoreModule.ExecuteQuery(Context.Guild.Id,
-          $"update settings set notifications = 'On';");
+                                $"UPDATE settings SET notifications = 'On';");
 
-        //Set embed content
-        var embed = CoreModule.SimpleEmbed(
-        Color.Green,
-        "Set notifications completed",
-        $"The **notifications** have been set **On**.");
+        //Create and set embed object.
+        var embed = CoreModule.SimpleEmbed(Color.Green,
+                                           "Set notifications completed",
+                                           $"The **notifications** have been set **`On`**.");
 
-        //Reply embed
+        //Reply embed.
         await ReplyAsync("", false, embed.Build());
 
         //Send notification
-        CoreModule.SendNotification(
-          Context.Guild.Id,
-          "Notification settings changed",
-          $"{Context.User.Mention} **changed** the Notification settings to **On**.");
+        CoreModule.SendNotification(Context.Guild.Id,
+                                    "Notification settings changed",
+                                    $"{Context.User.Mention} **changed** the Notification" +
+                                      $" settings to **`On`**.");
       }
+      //On false.
+      else {
+        //Send notification.
+        CoreModule.SendNotification(Context.Guild.Id,
+                                    "Notification settings changed",
+                                    $"{Context.User.Mention} **changed** the Notification" +
+                                      $" settings to **`Off`**.");
 
-      //On false
-      else
-      {
-
-        //Send notification
-        CoreModule.SendNotification(
-          Context.Guild.Id,
-          "Notification settings changed",
-          $"{Context.User.Mention} **changed** the Notification settings to **Off**.");
-
-        //Execute query
+        //Execute query.
         CoreModule.ExecuteQuery(Context.Guild.Id,
-          $"update settings set notifications = 'Off';");
+                                $"UPDATE settings SET notifications = 'Off';");
 
-        //Set embed content
-        var embed = CoreModule.SimpleEmbed(
-        Color.Green,
-        "Set notifications completed",
-        $"The **notifications** have been set **Off**.");
+        //Create and set embed content.
+        var embed = CoreModule.SimpleEmbed(Color.Green,
+                                           "Set notifications completed",
+                                           $"The **notifications** have been set **`Off`**.");
 
-        //Reply embed
+        //Reply embed.
         await ReplyAsync("", false, embed.Build());
       }
     }
