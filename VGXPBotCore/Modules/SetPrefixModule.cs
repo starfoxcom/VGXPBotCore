b589ed410b847b629af4462770e93f0ca76353f4
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2020-02-26 03:39:29 -0600

    Implement style guide to SetPrefixModule.cs

--- VGXPBotCore/Modules/SetPrefixModule.cs
+++ VGXPBotCore/Modules/SetPrefixModule.cs
@@ -12,37 +12,40 @@ using Discord.Addons.Interactive;
 
 using System.Data.SQLite;
 
-namespace VGXPBotCore.Modules
-{
+namespace VGXPBotCore.Modules {
+  /// <summary>
+  /// Class which contains commands to set the bot prefix.
+  /// </summary>
   public class SetPrefixModule : InteractiveBase
   {
-
+    /// <summary>
+    /// Command task which sets the bot prefix of the server.
+    /// </summary>
+    /// <param name="_prefix">The new bot prefix to set for the server.</param>
     [RequireUserPermission(GuildPermission.KickMembers, Group = "Permission")]
     [RequireOwner(Group = "Permission")]
     [Command("setprefix")]
     [Summary("`Sets` the prefix for the bot.")]
     [Alias("sp")]
-    public async Task SetPrefixAsync(string prefix)
+    public async Task
+    SetPrefixAsync(string _prefix)
     {
+      //Execute query.
+      CoreModule.ExecuteQuery(Context.Guild.Id, $"update settings set prefix = '{_prefix}';");
 
-      //Execute query
-      CoreModule.ExecuteQuery(Context.Guild.Id,
-        $"update settings set prefix = '{prefix}';");
+      //Set embed content.
+      var embed = CoreModule.SimpleEmbed(Color.Green,
+                                         "Set prefix completed",
+                                         $"The **prefix** **`{_prefix}`** has been **set**.");
 
-      //Set embed content
-      var embed = CoreModule.SimpleEmbed(
-      Color.Green,
-      "Set prefix completed",
-      $"The **prefix** **`{prefix}`** has been **set**.");
-
-      //Reply embed
+      //Reply embed.
       await ReplyAsync("", false, embed.Build());
 
-      //Send notification
-      CoreModule.SendNotification(
-        Context.Guild.Id,
-        "prefix changed",
-        $"{Context.User.Mention} **changed** the prefix to **`{prefix}`**.");
+      //Send notification.
+      CoreModule.SendNotification(Context.Guild.Id,
+                                  "prefix changed",
+                                  $"{Context.User.Mention} **changed** the prefix to" +
+                                    $" **`{_prefix}`**.");
     }
   }
 }
