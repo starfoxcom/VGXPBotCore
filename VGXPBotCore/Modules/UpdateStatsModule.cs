bb3049f6bf9d2deed42d700e7a07b6d69c1e9089
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2020-02-26 04:27:44 -0600

    Implement style guide to UpdateStatsModule.cs

--- VGXPBotCore/Modules/UpdateStatsModule.cs
+++ VGXPBotCore/Modules/UpdateStatsModule.cs
@@ -12,54 +12,59 @@ using Discord.Addons.Interactive;
 
 using System.Data.SQLite;
 
-namespace VGXPBotCore.Modules
-{
+namespace VGXPBotCore.Modules {
+  /// <summary>
+  /// Class which contains commands to update the database user stats.
+  /// </summary>
   public class UpdateStatsModule : InteractiveBase
   {
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="_xpfame">The new XP fame to set.</param>
+    /// <param name="_user">The Discord user to update.</param>
+    /// <remarks>
+    /// The user requires either be able to kick members on the server or be the bot owner.
+    /// </remarks>
     [RequireUserPermission(GuildPermission.KickMembers, Group = "Permission")]
     [RequireOwner(Group = "Permission")]
     [Command("updatestats")]
-    [Summary("`Updates` the actual XP of the user on the database.")]
+    [Summary("`Updates` the actual XP fame of the user on the database.")]
     [Alias("us")]
-    public async Task UpdateStatsAsync(int xpfame, [Remainder]SocketGuildUser user)
+    public async Task
+    UpdateStatsAsync(int _xpfame, [Remainder]SocketGuildUser _user)
     {
-
-      //On user on database
-      if (CoreModule.UserExistsServerDB(Context.Guild.Id, user.Id))
-      {
-
-        //Execute query
+      //On user on database.
+      if (CoreModule.UserExistsServerDB(Context.Guild.Id, _user.Id)) {
+        //Execute query.
         CoreModule.ExecuteQuery(Context.Guild.Id,
-          $"UPDATE users SET actualXP = {xpfame} where id = {user.Id};");
+                                $"UPDATE users SET actualXP = {_xpfame}" +
+                                  $" where id = {_user.Id};");
 
-        //Set embed object
-        var embed = CoreModule.SimpleEmbed(
-        Color.Green,
-        "Update completed",
-        $"The **update** of the user {user.Mention} is **completed**.");
+        //Create and set embed object.
+        var embed = CoreModule.SimpleEmbed(Color.Green,
+                                           "Update completed",
+                                           $"The **update** of the user {_user.Mention}" +
+                                             $" is **completed**.");
 
-        //Reply embed
+        //Reply embed.
         await ReplyAsync("", false, embed.Build());
 
-        //Send notification
-        CoreModule.SendNotification(
-          Context.Guild.Id,
-          "User stats updated",
-          $"{Context.User.Mention} **updated** the user {user.Mention} database stats.");
+        //Send notification.
+        CoreModule.SendNotification(Context.Guild.Id,
+                                    "User stats updated",
+                                    $"{Context.User.Mention} **updated** the user" +
+                                      $" {_user.Mention} database stats.");
       }
+      //On user not on database.
+      else {
+        //Create and set embed object.
+        var embed = CoreModule.SimpleEmbed(Color.Red,
+                                           "User not found",
+                                           $"{_user.Mention} doesn't exist on the database," +
+                                             $" **`update aborted`**.");
 
-      //On user not on database
-      else
-      {
-
-        //Create & set embed content
-        var embed = CoreModule.SimpleEmbed(
-        Color.Red,
-        "User not found",
-        $"{user.Mention} doesn't exist on the database, **`update aborted`**.");
-
-        //Reply embed
+        //Reply embed.
         await ReplyAsync("", false, embed.Build());
       }
     }
